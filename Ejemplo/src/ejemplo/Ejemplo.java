81c66b1393ee42ef55f70ab947986d9b1ee32cf6
JoseGuerrero3 <jxguerrero3@utpl.edu.ec>
2022-01-21 18:04:43 -0500

    ´´

--- Ejemplo/src/ejemplo/Ejemplo.java
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -11,11 +11,26 @@ package ejemplo;
  */
 public class Ejemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        int numeroInicial = 100;
+        int numeroFinal = 110;
+        
+        int[] datos = obtenerNumero(numeroInicial,numeroFinal);
+        
+        for(int i = 0; i<datos.length;i++){
+            System.out.printf("%d\n",datos[i]);
+        }
+        
+        
     }
-    
+    public static int[] obtenerNumero(int a,int b){
+        int[] numeros = new int[10];
+        
+        for(int i=0;i<numeros.length;i++){
+            numeros[i] = RandomUtils.nextInt(a, b);
+        }
+        
+        return numeros;
+}
 }
